d23c066bdfbfd78b3acdbde6d6ff6dfbfae146a6
Birame S. <birame.sene@zenika.com>
2024-06-03 00:39:38 +0200

    finished 1st step (dirty)

--- main.ts
+++ main.ts
@@ -4,12 +4,15 @@
 // deno-lint-ignore-file no-unused-vars
 export type HandSign = Rock | Paper | Scissors;
 type Rock = "pierre";
-type Paper = "papier";
+type Paper = "feuille";
 type Scissors = "ciseaux";
 // deno-lint-ignore no-unused-vars
 
 
 // deno-lint-ignore no-unused-vars
-export function winner(_player1: HandSign, _player2: HandSign): HandSign {
-  return "ciseaux"
+export function winner(player1: HandSign, player2: HandSign): HandSign {
+  if (player1 === "ciseaux" && player2 === "pierre" || player1 === "pierre" && player2 === "ciseaux") return "pierre"
+  if (player1 === "ciseaux" && player2 === "feuille" || player1 === "feuille" && player2 === "ciseaux") return "ciseaux"
+  if (player1 === "feuille" && player2 === "pierre" || player1 === "pierre" && player2 === "feuille") return "feuille"
+  return "pierre"
 }
