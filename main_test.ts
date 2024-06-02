54ce8a5824666beca1d767422369e33ffaf40d8f
Birame S. <birame.sene@zenika.com>
2024-06-03 00:16:28 +0200

    using expect assertions (BDD style)

--- main_test.ts
+++ main_test.ts
@@ -1,6 +1,10 @@
-import { assertEquals } from "jsr:@std/assert";
-import { type HandSign, winner } from "./main.ts";
+// deno-lint-ignore-file
+// deno-lint-ignore-file no-unused-vars
+import { expect } from "@std/expect";
+// deno-lint-ignore no-unused-vars
+import { winner } from "./main.ts";
 
 Deno.test("la pierre bat les ciseaux", () => {
-  assertEquals<HandSign>(winner("pierre", "ciseaux"), "pierre");
+  const expected: ReturnType<typeof winner> = "pierre"
+  expect(winner("pierre", "ciseaux")).toEqual(expected);
 });
