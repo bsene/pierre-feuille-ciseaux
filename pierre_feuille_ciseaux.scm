0afd5972272ec2afdad1ca7ab1feae0728fbfb5d
Birame S. <birame.sene@zenika.com>
2024-06-23 03:32:26 +0200

    step #1 is completed

--- pierre_feuille_ciseaux.scm
+++ pierre_feuille_ciseaux.scm
@@ -1,2 +1,44 @@
 (define nil '())
-(define (game p1 p2) 'rock)
+(define (scissors? hs)
+    (equal? 'scissors hs))
+
+(define (paper? hs)
+    (equal? 'paper hs))
+
+(define (rock? hs)
+    (equal? 'rock hs))
+
+(define (game p1 p2)
+    (cond
+        ((rock-compare p1 p2) (rock-compare p1 p2))
+        ((scissors-compare p1 p2) (scissors-compare p1 p2))
+        ((paper-compare p1 p2) (paper-compare p1 p2))
+       (else
+           nil)))
+
+
+(define (compare hs1 hs2 t? fn)
+   (if (t? hs1)
+       (fn hs2)
+       (if (t? hs2)
+           (fn hs1)
+           #f)))
+
+(define (rock-compare hs1 hs2)
+    (define (rock-again hs)
+        (if (scissors? hs) 'rock hs))
+    (compare hs1 hs2 rock? rock-again)
+   )
+
+
+(define (scissors-compare hs1 hs2)
+    (define (scissors-again hs)
+        (if (paper? hs) 'scissors hs))
+   (compare hs1 hs2 scissors? scissors-again))
+
+
+(define (paper-compare hs1 hs2)
+    (define (paper-again hs)
+        (if (rock? hs) 'paper hs))
+
+   (compare hs1 hs2 paper? paper-again))
--- pierre_feuille_ciseaux_test.scm
+++ pierre_feuille_ciseaux_test.scm
@@ -4,4 +4,22 @@
 
 
 (test-case "rock blunt scissors"
-    (assert-symbol= 'rock (game 'rock 'scissors)))
+    (assert-symbol= 'rock (game 'rock 'scissors))
+    (assert-symbol= 'rock (game 'scissors 'rock)))
+
+(test-case "scissors cut paper"
+    (assert-symbol= 'scissors (game 'scissors 'paper))
+    (assert-symbol= 'scissors (game 'paper 'scissors))
+    )
+
+(test-case "paper wrap rock"
+    (assert-symbol= 'paper (game 'paper 'rock))
+    (assert-symbol= 'paper (game 'rock 'paper)))
+
+
+(test-case "draw cases"
+
+    (assert-symbol= 'rock (game 'rock 'rock))
+    (assert-symbol= 'paper (game 'paper 'paper))
+    (assert-symbol= 'scissors (game 'scissors 'scissors))
+    )
